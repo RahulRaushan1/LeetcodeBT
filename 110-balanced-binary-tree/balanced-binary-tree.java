2b09eaa87e2c1bfd0640c237898ef70f72524054
Rahul Raushan <rahulraushan137138@gmail.com>
2025-09-04 05:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (23.14%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,17 +1,37 @@
-class Solution {
-    // comments
+// class Solution {
+//     // comments
+//     public int levels(TreeNode root){
+//         if(root==null) return 0;
+//         return 1 + Math.max(levels(root.left),levels(root.right));
+//     }
+//     public boolean isBalanced(TreeNode root) {
+//         if(root==null) return true;
+//         int diff = Math.abs(levels(root.left)-levels(root.right));
+//         if(diff>1) return false;
+//         boolean lst = isBalanced(root.left);
+//         if(lst==false) return false;
+//         boolean rst = isBalanced(root.right);
+//         if(rst==false) return false;
+//         return true;
+//     }
+// }
+
+
+ //Method 2--> Optimal Code
+  
+ class Solution{
+    static boolean ans;
     public int levels(TreeNode root){
         if(root==null) return 0;
-        return 1 + Math.max(levels(root.left),levels(root.right));
+        int leftLevels = levels(root.left);
+        int rightLevels = levels(root.right);
+        int diff = Math.abs(leftLevels - rightLevels);
+        if(diff>1) ans = false;
+        return 1 + Math.max(leftLevels,rightLevels);
     }
-    public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        int diff = Math.abs(levels(root.left)-levels(root.right));
-        if(diff>1) return false;
-        boolean lst = isBalanced(root.left);
-        if(lst==false) return false;
-        boolean rst = isBalanced(root.right);
-        if(rst==false) return false;
-        return true;
+    public boolean isBalanced(TreeNode root){
+        ans = true;
+        levels(root); // TC = O(n)
+        return ans;
     }
-}
+ } 
